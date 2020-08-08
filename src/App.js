007b64f9955e6c5c163815258a8d09b4465d6e8c
Azhar Zaman <68331974+drcraze@users.noreply.github.com>
2020-08-08 19:46:48 +0500

    Setting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faWindowClose, faGift, } from '@fortawesome/free-solid-svg-icons'
+import My from './my.svg'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FontAwesomeIcon className='Icon' icon={faCoffee} color='blue' />
+      <FontAwesomeIcon className='Icon' icon={faGift} color='blue' />
+      <img className='SVG' src={My} alt="" />
     </div>
   );
 }
