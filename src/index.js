8a7e1c39e9b976bcfa3b13d1a8007e63007df4d3
Azhar Zaman <68331974+drcraze@users.noreply.github.com>
2020-11-08 09:30:15 +0500

    Done

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-'use strict';
+"use strict";
 
-var _react = require('react');
+var _react = require("react");
 
 var _react2 = _interopRequireDefault(_react);
 
-var _reactDom = require('react-dom');
+var _reactDom = require("react-dom");
 
 var _reactDom2 = _interopRequireDefault(_reactDom);
 
-require('./main.css');
+require("./main.css");
 
-require('./bg.css');
+require("./bg.css");
 
-var _serviceWorker = require('./serviceWorker');
+require("./fonts.css");
+
+var _serviceWorker = require("./serviceWorker");
 
 var serviceWorker = _interopRequireWildcard(_serviceWorker);
 
@@ -20,29 +22,33 @@ function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj;
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+// babel src/App.js --out-file=src/index.js --presets=react,env --watch
+
 var RootComponent = function RootComponent() {
   return _react2.default.createElement(
-    'div',
-    { className: 'Root-container' },
+    "div",
+    { className: "Root-container" },
     _react2.default.createElement(
-      'h1',
-      { className: 'Heading' },
-      'Lets Do It'
+      "h1",
+      { className: "Heading" },
+      "WELCOME TO"
     ),
     _react2.default.createElement(
-      'p',
-      { className: 'Para' },
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere ad illum, veniam molestias praesentium minus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere ad illum, veniam molestias praesentium minus.'
+      "p",
+      { className: "Para" },
+      "The most practical, friendly and Co-operative team on Upwork capable of providing optimal productivity. ",
+      _react2.default.createElement("br", null),
+      "We commit if we can Hit"
     ),
     _react2.default.createElement(
-      'button',
-      { className: 'Btn', href: 'https://googel.com' },
-      'Contact'
+      "h1",
+      { className: "Heading2" },
+      "WE BELIEVE & YOU WILL"
     )
   );
 };
 
-var RootDirectory = document.getElementById('Root');
+var RootDirectory = document.getElementById("Root");
 
 _reactDom2.default.render(_react2.default.createElement(RootComponent, null), RootDirectory);
 
--- src/main.js
+++ src/main.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './main.css';
-import './bg.css';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./main.css";
+import "./bg.css";
+import "./fonts.css";
+import * as serviceWorker from "./serviceWorker";
 
+// babel src/App.js --out-file=src/index.js --presets=react,env --watch
 
-let RootComponent=() => {
+let RootComponent = () => {
   return (
     <div className="Root-container">
-       <h1 className='Heading'>Lets Do It</h1>
-       <p className='Para'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere ad illum, veniam molestias praesentium minus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere ad illum, veniam molestias praesentium minus.</p>
-       <button className='Btn' href='https://googel.com'>Contact</button>
+      <h1 className="Heading">WELCOME TO</h1>
+      <p className="Para">
+        The most practical, friendly and Co-operative team on Upwork capable of
+        providing optimal productivity. <br></br>
+        We commit if we can Hit
+      </p>
+      <h1 className="Heading2">WE BELIEVE & YOU WILL</h1>
+
+      {/* <button className='Btn' href='https://google.com'>Contact</button> */}
     </div>
   );
-}
+};
 
-let RootDirectory= document.getElementById('Root')
+let RootDirectory = document.getElementById("Root");
 
-ReactDOM.render(
-  <RootComponent />, RootDirectory
-);
+ReactDOM.render(<RootComponent />, RootDirectory);
 
 serviceWorker.register();
